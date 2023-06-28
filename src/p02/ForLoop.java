09554b8143978822327ad63265ff199c6c3eea4e
LeeWanggyun <wanggyun99@naver.com>
2023-06-28 09:35:05 +0900

    test

---
+++ src/p02/ForLoop.java
@@ -0,0 +1,17 @@
+package p02;
+
+public class ForLoop {
+	
+	public static void main(String[] args) {
+	
+		for(int i=1; i<10; i++) {
+		System.out.println(1+ "x" + i + "=" + i);
+		}
+		
+		for(int i=1; i<10; i++) {
+			System.out.println(i + "x" + 1 + "=" + i);
+		}
+		
+	}
+
+}
